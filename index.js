884c5a5280746c99a345460d5a66d20ca76f2b94
Junior Rocha <rochajunior93@hotmail.com>
2021-02-11 18:24:06 -0300

    added condition

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-const PORT = 3000
+const express = require('express');
+const app = express();
+app.use(express.json());
+const PORT = 3333;
 
-const projects = [] 
+const projects = []; 
 // Middleware que verifica se o projeto ja existe
 function checkProjectExists(req, res, next) {
   const { id } = req.params;
@@ -22,35 +22,35 @@ app.use(logRequests);
 
 //criacão de um novo projeto
 app.post('/projects', (req, res)=> {
-  const {id, title} = req.body
+  const {id, title} = req.body;
   const project = {
     id,
     title,
     tasks:[]
   }
-  projects.push(project)
-  return res.json(project)
-})
+  projects.push(project);
+  return res.json(project);
+});
 
 //Listar projetos
 app.get('/projects', (req, res)=> {
-  return res.json(projects)
-})
+  return res.json(projects);
+});
 // Altera o titulo do projeto utilizando o id
 app.put('/projects/:id',checkProjectExists,(req, res)=> {
- const {id} = req.params
- const {title} = req.body
+ const {id} = req.params;
+ const {title} = req.body;
  const project = projects.find(p => p.id == id);
  project.title = title;
  return res.json(project);
-})
+});
 // Deleta o projeto
 app.delete('/projects/:id',checkProjectExists,(req, res)=> {
- const {id} = req.params
+ const {id} = req.params;
  const projectId = projects.findIndex(p => p.id == id);
- projects.splice(projectId, 1)
- res.json({Message:"Deletado com sucesso"})
-})
+ projects.splice(projectId, 1);
+ res.json({Message:"Deletado com sucesso"});
+});
 // Adiciona Tarefa no projeto pelo id
 app.post('/projects/:id/tasks',checkProjectExists,(req, res)=> {
   const { id } = req.params;
@@ -58,8 +58,12 @@ app.post('/projects/:id/tasks',checkProjectExists,(req, res)=> {
   const project = projects.find(p => p.id == id);
   project.tasks.push(title);
   return res.json(project);
-})
+});
 
-app.listen(PORT, () =>{
-  console.log(`Server started on port ${PORT}`)
-})
+app.listen(PORT, (err) =>{
+  if(err) {
+  console.log(`Ops error!`);
+  } else{
+  console.log(`Server started on port ${PORT}`);
+  }
+});
